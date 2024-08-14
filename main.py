a70ec2eb40900362367af32ab02860f31b158edd
Gustavo <gugams9191@gmail.com>
2024-08-14 14:57:36 -0300

    1

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 import tkinter as tk
-from functions import show_admin_menu, show_visitor_menu, load_users
-
-def handle_login(username, password, root):
-    users = load_users()
-    username = username.lower()
-
-    if username == "hp" and password == "HP@33993399":
-        show_admin_menu(root, username)
-    elif username in users["usuarios"] and users["usuarios"][username]["password"] == password:
-        show_visitor_menu(root, username)
-    else:
-        tk.messagebox.showerror("Erro", "Usuário ou senha incorretos!")
+from functions import show_login_screen, handle_login
 
 # Executa a aplicação
 if __name__ == "__main__":
@@ -19,7 +8,6 @@ if __name__ == "__main__":
     root.configure(bg="black")
     root.state('zoomed')
     
-    from functions import show_login_screen
     show_login_screen(root)
     
     root.mainloop()
